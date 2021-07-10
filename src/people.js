cd8e34d81f0158a37056241d2cc28ca88ff1a1ed
Kishor Ganeshrao <kishorkg@yahoo.com>
2021-07-10 18:01:02 +1200

    addition of eslint

--- src/people.js
+++ src/people.js
@@ -1,13 +1,13 @@
 export let people = [{
-    name: "John",
-    age: 35,
-    hairColor: "brown"
+  name: "John",
+  age: 35,
+  hairColor: "brown"
 }, {
-    name: "Joe",
-    age: 20,
-    hairColor: "blonde"
+  name: "Joe",
+  age: 20,
+  hairColor: "blonde"
 }, {
-    name: "Jane",
-    age: 40,
-    hairColor: "red"
+  name: "Jane",
+  age: 40,
+  hairColor: "red"
 }]
